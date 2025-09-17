a328e7539d0891a4524a3617329ab564c7d65609
Anandu0603 <anandusuresh510@gmail.com>
2025-09-18 02:00:34 +0530

    Fix merge conflicts in src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
@@ -8,15 +7,4 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-=======
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
->>>>>>> c20b5b22cdf69db7ad6801e0e59f05348adb12a2
 );
