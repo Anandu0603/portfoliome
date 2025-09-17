38c6e2fb064f8246826a40ecccb929835a1f1d8a
Anandu0603 <anandusuresh510@gmail.com>
2025-09-18 02:09:51 +0530

    Fix PostCSS config: Remove Git merge conflict markers causing Netlify build failure

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 export default {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
     },
-=======
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
->>>>>>> c20b5b22cdf69db7ad6801e0e59f05348adb12a2
 };
